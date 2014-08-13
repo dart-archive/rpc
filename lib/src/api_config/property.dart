fd52983a0a9b5af889d0ff64213b07b5c7113f7c
Gerwin Sturm <scarygami@gmail.com>
2014-08-13 03:18:16 +0200

    First property type checks

--- lib/src/api_config/property.dart
+++ lib/src/api_config/property.dart
@@ -5,10 +5,33 @@ class ApiConfigSchemaProperty {
   String _propertyName;
   TypeMirror _type;
   ApiConfigSchema _ref;
+  bool _repeated = false;
+  String _apiType;
+  String _apiFormat;
 
-  ApiConfigSchemaProperty(this._property) {
+  ApiConfigSchemaProperty(this._property, String schemaName) {
     _propertyName = MirrorSystem.getName(_property.simpleName);
     _type = _property.type;
+    if(_type.simpleName == #dynamic) {
+      throw new ApiConfigError('${schemaName}.${_propertyName}: Property needs to have a type defined.');
+    }
+    if (_type.isSubtypeOf(reflectType(List))) {
+      _repeated = true;
+      var types = _type.typeArguments;
+      if (types.length != 1) {
+        throw new ApiConfigError('${schemaName}.${_propertyName}: List property must specify exactly one type parameter');
+      }
+      _type = types[0];
+      if(_type.simpleName == #dynamic) {
+        throw new ApiConfigError('${schemaName}.${_propertyName}: List property must specify exactly one type parameter');
+      }
+    }
+
+    // TODO: determine valid type or reference
+
+    if (_ref == null && _apiType == null) {
+      throw new ApiConfigError('${schemaName}.${_propertyName}: Invalid type.');
+    }
   }
 
   String get propertyName => _propertyName;
