33646a763e7376aabbc6d3d14364b9b51c1c5ecb
Scarygami <scarygami@gmail.com>
2014-08-22 12:28:04 +0200

    Complete ApiProperty class (not implemented in api_config yet)

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -32,17 +32,47 @@ class ApiMethod {
  * Optional annotation for parameters inside of API request/response messages.
  */
 class ApiProperty {
+  /// description of the property to be included in the discovery document
   final String description;
 
+  /**
+   * Specifies the representation of int and double properties in the backend
+   * 
+   * Possible values for int: 'int32' (default), 'uint32', 'int64', 'uint64'
+   * 
+   * The 64 bit values will be represented as String in the JSON requests/responses
+   * 
+   * Possible values for double: 'double' (default), 'float'
+   */
   final String variant;
 
+  /// Whether the property needs to be supplied for requests
+  final bool required;
+  
+  /// Default value for this property if it's not supplied in the request
+  final defaultValue;
+  
+  /// For int properties: the minimal value allowed in requests
+  final int minValue;
+  
+  /// For int properties: the maximal value allowed in requests
+  final int maxValue;
+  
   /**
    * Possible values for enum properties, as value - description pairs.
    * Properties using this will have to be String
    */
   final Map<String, String> values;
 
-  const ApiProperty({this.description, this.variant, this.values});
+  const ApiProperty({
+    this.description,
+    this.variant,
+    this.required: false,
+    this.defaultValue,
+    this.minValue,
+    this.maxValue,
+    this.values}
+  );
 
   // TODO: add default, required, min/max values
 }
