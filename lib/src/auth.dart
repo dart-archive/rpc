2157025fb4a1251541ba306172d08b0b9d29898a
Gerwin Sturm <scarygami@gmail.com>
2014-09-13 02:54:23 +0200

    Switch OAuth2 library to new one

--- lib/src/auth.dart
+++ lib/src/auth.dart
@@ -5,8 +5,9 @@ import 'errors.dart';
 import 'dart:async';
 
 import 'package:appengine/appengine.dart';
-import 'package:google_oauth2_v2_api/oauth2_v2_api_console.dart';
-import 'package:google_oauth2_v2_api/oauth2_v2_api_client.dart';
+import 'package:googleapis/oauth2/v2.dart';
+import 'package:http/http.dart';
+
 
 List<String> _authSchemes = ['OAUTH', 'BEARER'];
 
@@ -28,30 +29,33 @@ Future<ApiUser> checkAuth(String authHeader, List<String> clientIds) {
     return new Future.error(new UnauthorizedError('Invalid Authorization header'));
   }
 
-  var client = new Oauth2();
+  var httpClient = new IOClient();
+  var apiClient = new Oauth2Api(httpClient);
   var token = auth_parts[1];
   var completer = new Completer();
   var request;
   if (token.startsWith('ya29.') || token.startsWith('1/')) {
     context.services.logging.info('Checking access token');
-    request = client.tokeninfo(access_token: token);
+    request = apiClient.tokeninfo(accessToken: token);
   } else {
     // TODO: check the jwt token directly?
     context.services.logging.info('Checking ID token');
-    request = client.tokeninfo(id_token: token);
+    request = apiClient.tokeninfo(idToken: token);
   }
   request
     .then((Tokeninfo info) {
+      httpClient.close();
       context.services.logging.debug('Token Info retrieved successfully: ${info.toString()}');
-      if (!clientIds.contains(info.issued_to)) {
+      if (!clientIds.contains(info.issuedTo)) {
         context.services.logging.info('Client ID not allowed for this API');
         completer.complete(null);
         return;
       }
       // TODO: memcache the result for quicker future results? potential security issue?
-      completer.complete(new ApiUser(info.user_id, info.email));
+      completer.complete(new ApiUser(info.userId, info.email));
     })
     .catchError((e) {
+      httpClient.close();
       context.services.logging.error('Failed to verify authorization token: $e');
       completer.completeError(new UnauthorizedError('Failed to verify authorization token: $e'));
       return true;
